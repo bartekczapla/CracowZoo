56a0492e42b7e55736478114354ba8e353a21a55
Bartek <bartlomiej.czapla@globallogic.com>
2020-07-03 13:41:51 +0200

    EnumConverter

---
+++ CracowZoo/CracowZoo/CracowZoo/Converters/EnumConverter.cs
@@ -0,0 +1,38 @@
+﻿using CracowZoo.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using static CracowZoo.Helpers.EnumHelper;
+
+namespace CracowZoo.Converters
+{
+    public class EnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is AnimalDanger)
+            {
+                return ((AnimalDanger)value).GetDescription();
+            }
+            else if (value is AnimalGroup)
+            {
+                return ((AnimalGroup)value).GetDescription();
+            }
+            else if (value is ConservationStatus)
+            {
+                return ((ConservationStatus)value).GetDescription();
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
--- CracowZoo/CracowZoo/CracowZoo/Enums/AnimalDanger.cs
+++ CracowZoo/CracowZoo/CracowZoo/Enums/AnimalDanger.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace CracowZoo.Enums
 {
     public enum AnimalDanger
     {
+        [Description("Brak")]
         None,
+        [Description("Srednie")]
         Medium,
+        [Description("Wysokie")]
         High
     }
 }
