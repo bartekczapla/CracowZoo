2c99c2c664c6dc0d204632afe7731613d64038ce
mkopacz94 <m.kopacz94@gmail.com>
2020-07-04 14:19:04 +0200

    Added tidbits to animal details page

--- CracowZoo/CracowZoo/CracowZoo/ViewModels/AnimalDetailsPageViewModel.cs
+++ CracowZoo/CracowZoo/CracowZoo/ViewModels/AnimalDetailsPageViewModel.cs
@@ -12,16 +12,19 @@ namespace CracowZoo.ViewModels
     public class AnimalDetailsPageViewModel : ViewModelBase
     {
         private Animal _selectedAnimal;
-        public Animal SelectedAnimal { 
-            get
-            {
-                return _selectedAnimal;
-            }
-            set
-            {
-                SetProperty(ref _selectedAnimal, value);
-            }
+        public Animal SelectedAnimal 
+        { 
+            get => _selectedAnimal;
+            set => SetProperty(ref _selectedAnimal, value);
         }
+
+        private ICollection<AnimalTidbit> _animalTidbits;
+        public ICollection<AnimalTidbit> AnimalTidbits
+        {
+            get => _animalTidbits;
+            set => SetProperty(ref _animalTidbits, value);
+        }
+
         public AnimalDetailsPageViewModel(INavigationService navigationService)
         : base(navigationService)
         {
@@ -30,6 +33,7 @@ namespace CracowZoo.ViewModels
         public override void OnNavigatedTo(INavigationParameters parameters)
         {
             SelectedAnimal = parameters.GetValue<Animal>("selectedAnimal");
+            AnimalTidbits = SelectedAnimal.AnimalTidbits;
             base.OnNavigatedTo(parameters);
         }
     }
