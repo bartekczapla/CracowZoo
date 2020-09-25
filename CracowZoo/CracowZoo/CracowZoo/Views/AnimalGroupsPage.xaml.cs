9e1548e14657680d88f00fb94ec2e170c1ba4b38
mkopacz94 <47296347+mkopacz94@users.noreply.github.com>
2020-09-26 00:12:12 +0200

    Some minor view changes

--- CracowZoo/CracowZoo/CracowZoo/Views/AnimalGroupsPage.xaml.cs
+++ CracowZoo/CracowZoo/CracowZoo/Views/AnimalGroupsPage.xaml.cs
@@ -13,5 +13,18 @@ namespace CracowZoo.Views
         {
             FoundAnimalsListView.IsVisible = e.NewTextValue.Length > 0;
         }
+
+        private void SearchEntry_Unfocused(object sender, FocusEventArgs e)
+        {
+            FoundAnimalsListView.IsVisible = false;
+        }
+
+        private void SearchEntry_Focused(object sender, FocusEventArgs e)
+        {
+            if (string.IsNullOrEmpty(SearchEntry?.Text))
+                return;
+
+            FoundAnimalsListView.IsVisible = SearchEntry.Text.Length > 0;
+        }
     }
 }
