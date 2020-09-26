e3793b0f19ecbfc00bb657d140d06da65b6698dd
mkopacz94 <47296347+mkopacz94@users.noreply.github.com>
2020-09-26 20:47:43 +0200

    ScrollView bar is now invisible

---
+++ CracowZoo/CracowZoo/CracowZoo.Android/CustomRenderers/ScrollViewNoBarRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.ScrollView), typeof(CracowZoo.Droid.CustomRenderers.ScrollViewNoBarRenderer))]
+namespace CracowZoo.Droid.CustomRenderers
+{
+    public class ScrollViewNoBarRenderer : ScrollViewRenderer
+    {
+        public ScrollViewNoBarRenderer(Context context) : base(context)
+        {
+
+        }
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || this.Element == null)
+                return;
+
+            if (e.OldElement != null)
+                e.OldElement.PropertyChanged -= OnElementPropertyChanged;
+
+            e.NewElement.PropertyChanged += OnElementPropertyChanged;
+        }
+
+        protected void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            this.HorizontalScrollBarEnabled = false;
+            this.VerticalScrollBarEnabled = false;
+        }
+    }
+}
