555e0ea79a33426621d68fc8c605ed485aa6dae1
mkopacz94 <47296347+mkopacz94@users.noreply.github.com>
2020-09-27 10:57:19 +0200

    Added custom entry renderer (corner radius, no underline)

---
+++ CracowZoo/CracowZoo/CracowZoo.Android/CustomRenderers/EntryNoUnderlineRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+using Android.Content;
+using CracowZoo.Views.CustomControls;
+using CracowZoo.Droid.CustomRenderers;
+using System.Drawing;
+
+[assembly: ExportRenderer(typeof(EntryNoUnderline), typeof(EntryNoUnderlineRenderer))]
+namespace CracowZoo.Droid.CustomRenderers
+{
+    public class EntryNoUnderlineRenderer : EntryRenderer
+    {
+        public EntryNoUnderlineRenderer(Context context) : base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.SetPadding(50, 30, 50, 30);
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetColor(Android.Graphics.Color.White);
+                gd.SetCornerRadius(100);
+                gd.SetStroke(2, Android.Graphics.Color.Gray);
+                Control.Background = gd;
+            }
+        }
+    }
+}
---
+++ CracowZoo/CracowZoo/CracowZoo/Views/CustomControls/EntryNoUnderline.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CracowZoo.Views.CustomControls
+{
+    public class EntryNoUnderline : Entry
+    {
+    }
+}
